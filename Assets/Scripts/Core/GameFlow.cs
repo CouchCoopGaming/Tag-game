9b109ed81ca465484ecd91fd0ed47aaf30dd2360
Landon Sykes <CouchCoopGaming@users.noreply.github.com>
2026-09-24 19:30:27 -0500

    UX: KeypadEnter confirms mode-select row

--- Assets/Scripts/Core/GameFlow.cs
+++ Assets/Scripts/Core/GameFlow.cs
@@ -408,7 +408,8 @@ namespace Tag.Core
                 if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha4)) SetFocus(ref _menuCursor, 3);
                 if (UnityEngine.Input.GetKeyDown(KeyCode.UpArrow)) Nudge(ref _menuCursor, -1, 3);
                 if (UnityEngine.Input.GetKeyDown(KeyCode.DownArrow)) Nudge(ref _menuCursor, 1, 3);
-                if (UnityEngine.Input.GetKeyDown(KeyCode.Return) || UnityEngine.Input.GetKeyDown(KeyCode.Space))
+                if (UnityEngine.Input.GetKeyDown(KeyCode.Return) || UnityEngine.Input.GetKeyDown(KeyCode.KeypadEnter) ||
+                    UnityEngine.Input.GetKeyDown(KeyCode.Space))
                     ConfirmModeAndPlay();
             }
             else if (State == GameFlowState.RoundEnd)
